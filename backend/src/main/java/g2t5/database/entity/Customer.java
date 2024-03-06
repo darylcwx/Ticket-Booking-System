99596a9cb81144dd738df5a0aa09d44878f26287
malmalcolm <92280886+malmalcolm@users.noreply.github.com>
2024-03-07 00:27:41 +0800

    Customer.java skeleton

---
+++ backend/src/main/java/g2t5/database/entity/Customer.java
@@ -0,0 +1,22 @@
+package g2t5.database.entity;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Customer extends User {
+
+  private double account_balance;
+  private String payment_details;
+  private ArrayList<booking> booking_history;
+
+  public String create_booking() {
+    //
+  }
+  public booking get_booking_history() {
+    //
+  }
+
+  public void cancel_booking() {
+    //
+  }
+}
