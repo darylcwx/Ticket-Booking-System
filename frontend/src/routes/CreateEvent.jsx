adb310e8093211ffd24e253072684390bf14293c
bellekwang <kwangkaixuan@gmail.com>
2024-03-14 17:45:26 +0800

    update: create event link to BE

--- frontend/src/routes/CreateEvent.jsx
+++ frontend/src/routes/CreateEvent.jsx
@@ -9,9 +9,53 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 export default function Dashboard() {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [discountAvailable, setDiscountAvailable] = useState(false);
+    const [eventName, setEventName] = useState();
+    const [eventVenue, setEventVenue] = useState();
+    const [eventDesc, setEventDesc] = useState();
+    const [totalTicketNum, setTotalTicketNum] = useState();
+    const [maxGuestNum, setMaxGuestNum] = useState();
+    const [ticketPrice, setTicketPrice] = useState();
+    const [cancellationFee, setCancellationFee] = useState();
+    const [discountMinTix, setDiscountMinTix] = useState();
+    const [discountPercentage, setDiscountPercentage] = useState();
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Construct the event object
+        const event = {
+            name: eventName,
+            venue: eventVenue,
+            description: eventDesc,
+            dateTime: selectedDate,
+            ticketsAvailable: totalTicketNum,
+            guestsAllowed: maxGuestNum,
+            ticketPrice: ticketPrice,
+            cancellationFee: cancellationFee,
+            discountAvailable: discountAvailable,
+            discountMinTix: discountMinTix,
+            discountPercentage: discountPercentage
+        };
+
+        try {
+            const response = await fetch('http://localhost:8080/add-event', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(event)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to create event');
+            }
+            console.log('Event created successfully');
+        } catch (error) {
+            console.error('Error creating event:', error.message);
+        }
     };
     
     useEffect(() => {
@@ -24,7 +68,7 @@ export default function Dashboard() {
             <Container className="pt-[65px]">
             <h1 className="text-white text-3xl mt-[40px]">Create Event</h1>
                 <Container className="bg-gray-50 p-5 mt-[20px] mb-[60px] w-auto">
-                    <form className="w-full">
+                    <form className="w-full" onSubmit={handleSubmit}>
 
                         {/* EVENT NAME */}
                         <div className="flex flex-wrap -mx-3 mb-3">
@@ -32,7 +76,30 @@ export default function Dashboard() {
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventName">
                                     Event Name
                                 </label>
-                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="eventName" type="text" placeholder="E.g. Taylor Swift | The Eras Tour"/>
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    id="eventName" type="text" 
+                                    placeholder="E.g. Taylor Swift | The Eras Tour"
+                                    value={eventName}
+                                    onChange={(e) => setEventName(e.target.value)}
+                                />
+                            </div>
+                        </div>
+
+                        {/* EVENT NAME */}
+                        <div className="flex flex-wrap -mx-3 mb-3">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventName">
+                                    Description
+                                </label>
+                                <textarea 
+                                    id="message" rows="4" 
+                                    class="block p-2.5 w-full text-sm bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    placeholder="Write your thoughts here..." 
+                                    value={eventDesc}
+                                    onChange={(e) => setEventDesc(e.target.value)}
+                                >
+                                </textarea>                           
                             </div>
                         </div>
 
@@ -40,9 +107,15 @@ export default function Dashboard() {
                         <div className="flex flex-wrap -mx-3 mb-3">
                             <div className="w-full px-3">
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventVenue">
-                                    Event Venue
+                                    Venue
                                 </label>
-                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="eventVenue" type="text" placeholder="E.g. Singapore National Stadium"/>
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    id="eventVenue" type="text" 
+                                    placeholder="E.g. Singapore National Stadium"
+                                    value={eventVenue}
+                                    onChange={(e) => setEventVenue(e.target.value)}
+                                />
                             </div>
                         </div>
 
@@ -50,7 +123,7 @@ export default function Dashboard() {
                         <div className="flex flex-wrap -mx-3 mb-6">
                             <div className="w-full px-3">
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="eventDatetime">
-                                    Event Date & time
+                                    Date & time
                                 </label>
                                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                                     <DateTimePicker
@@ -63,13 +136,18 @@ export default function Dashboard() {
                         </div>
 
                         <div className="flex flex-wrap -mx-3 mb-6">
-                            
                             {/* TICKETS AVAILABLE */}
                             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="totalTicketNum">
                                     Tickets Available
                                 </label>
-                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="totalTicketNum" type="number" placeholder="40"/>
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    id="totalTicketNum" type="number" 
+                                    placeholder="40"
+                                    value={totalTicketNum}
+                                    onChange={(e) => setTotalTicketNum(parseInt(e.target.value))}
+                                />
                             </div>
 
                             {/* MAX GUEST ALLOWED */}
@@ -77,7 +155,12 @@ export default function Dashboard() {
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="maxGuestNum">
                                     Max Guest Allowed
                                 </label>
-                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="maxGuestNum" type="number" placeholder="5"/>
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    id="maxGuestNum" type="number" placeholder="5"
+                                    value={maxGuestNum}
+                                    onChange={(e) => setMaxGuestNum(parseInt(e.target.value))}
+                                    />
                             </div>
                         </div>
 
@@ -90,7 +173,12 @@ export default function Dashboard() {
                                 </label>
                                 <div className="flex items-center">
                                     <span className="mr-2">$</span>
-                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ticketPrice" type="number" placeholder="123"/>
+                                    <input 
+                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                        id="ticketPrice" type="number" placeholder="123"
+                                        value={ticketPrice}
+                                        onChange={(e) => setTicketPrice(parseFloat(e.target.value))}    
+                                    />
                                 </div>
                             </div>
 
@@ -101,7 +189,12 @@ export default function Dashboard() {
                                 </label>
                                 <div className="flex items-center">
                                     <span className="mr-2">$</span>
-                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="cancellationFee" placeholder="23"/>
+                                    <input 
+                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                        id="grid-city" type="cancellationFee" placeholder="23"
+                                        value={cancellationFee}
+                                        onChange={(e) => setCancellationFee(parseFloat(e.target.value))}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -135,7 +228,12 @@ export default function Dashboard() {
                                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="discountMinTix">
                                             Minimum Ticket Purchase
                                         </label>
-                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="discountMinTix" type="number" placeholder="123"/>
+                                        <input 
+                                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                            id="discountMinTix" type="number" placeholder="123"
+                                            value={discountMinTix}
+                                            onChange={(e) => setDiscountMinTix(parseInt(e.target.value))}
+                                        />
                                     </div>
 
                                     {/* DISCOUNT PERCENTAGE */}
@@ -144,7 +242,12 @@ export default function Dashboard() {
                                             Discount
                                         </label>
                                         <div className="flex items-center">
-                                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="discountPercentage" type="number" placeholder="123"/>
+                                            <input 
+                                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                                id="discountPercentage" type="number" placeholder="123"
+                                                value={discountPercentage}
+                                                onChange={(e) => setDiscountPercentage(parseInt(e.target.value))}    
+                                            />
                                             <span className="ml-2">%</span>
                                         </div>
                                     </div>
@@ -154,7 +257,7 @@ export default function Dashboard() {
 
                         {/* SUBMIT BUTTON */}
                         <div className="flex justify-end">
-                            <Button variant="contained">Create Event</Button>
+                            <Button type="submit" variant="contained">Create Event</Button>
                         </div>
                     </form>
                 </Container>
