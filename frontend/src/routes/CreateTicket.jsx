059a41040e6274141b9fe569de817d466179dd9f
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2024-04-09 02:04:13 +0800

    createTicket page (incomplete)
    handleSubmit form

--- frontend/src/routes/CreateTicket.jsx
+++ frontend/src/routes/CreateTicket.jsx
@@ -1,23 +1,231 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import DocumentTitle from "../components/DocumentTitle";
-
 import Navbar from "../components/Navbar";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
+import { InputLabel, TextField, Box, Button, Container, Chip, Alert } from "@mui/material";
+import dayjs from "dayjs";
+import sendEmail from "../utils/sendEmail";
 
-export default function createTicket() {
+export default function CreateTicket() {
     DocumentTitle("Create Ticket");
     const navigate = useNavigate();
+    const [name, setName] = useState();
+    const [nameError, setNameError] = useState("");
+    const [email, setEmail] = useState();
+    const [emailError, setEmailError] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [event, setEvent] = useState({});
+    const [eventName, setEventName] = useState();
+    const [eventVenue, setEventVenue] = useState();
+    const [ticketPrice, setTicketPrice] = useState();
+    const [showAlert, setShowAlert] = useState(false);
+    const [users, setUsers] = useState({});
 
+    useEffect(() => {
+        const eventId = location.pathname.split("/")[2];
+        const fetchEvent = async (eventId) => {
+            try {
+                const response = await fetch(
+                    `http://localhost:8080/event/${eventId}`,
+                    {
+                        method: "GET",
+                        headers: { "Content-Type": "application/json" },
+                    }
+                );
+                const data = await response.json();
+                console.log(data);
+                setEvent(data);
+                setEventName(data.name);
+                setEventVenue(data.venue);
+                setSelectedDate(dayjs(data.datetime));
+                setTicketPrice(data.ticketPrice);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchEvent(eventId);
+    }, []);
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+        if (e.target.value.length < 3) {
+            setNameError("Name must be at least 3 characters long");
+        } else if (e.target.value.length > 20) {
+            setNameError("Name must be less than 20 characters long");
+        } else if (!/^[a-zA-Z ]+$/.test(e.target.value)) {
+            setNameError("Name must contain only letters and spaces");
+        } else {
+            setNameError(false);
+        }
+    };
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+        if (!/^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/.test(e.target.value)) {
+            setEmailError("Invalid email address");
+        } else {
+            setEmailError(false);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (nameError || emailError) {
+            alert("Form is invalid! Please check the fields...");
+        } else {
+            alert("Form is valid! Submitting the form...");
+        }
+    }
 
     return (
         <div className="bg-main w-screen h-full">
             <Navbar />
             <Container className="pt-[65px]">
-                <h1 className="text-white text-3xl mt-[40px]">Create Ticket</h1>
+                <div className="flex items-end gap-x-3">
+                    <h1 className="text-white text-3xl mt-[40px]">
+                        Create Ticket
+                    </h1>
+                </div>
                 <Container className="bg-gray-50 p-5 mt-[20px] mb-[60px] w-auto">
-                    
+                    {/* <form className="w-full" onSubmit={handleSubmit}> */}
+                    <Box component="form" onSubmit={handleSubmit} noValidate>
+
+                        {/* CUSTOMER NAME */}
+                        <div className="flex flex-wrap -mx-3 mb-5">
+                            <div className="w-full px-3">
+                                <TextField
+                                    required
+                                    className="appearance-none block w-full text-gray-700 border ${errorMessage ? 'border-red-500' : 'border-gray-200'} rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="name"
+                                    type="text"
+                                    label="Name"
+                                    value={name}
+                                    onChange={handleNameChange}
+                                    error={nameError}
+                                    helperText={nameError}
+                                />
+                            </div>
+                        </div>
+
+                        {/* CUSTOMER EMAIL */}
+                        <div className="flex flex-wrap -mx-3 mb-5">
+                            <div className="w-full px-3">
+                                <TextField
+                                    required
+                                    className="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="email"
+                                    type="text"
+                                    label="Email"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    error={emailError}
+                                    helperText={emailError}
+                                />
+                            </div>
+                        </div>
+
+                        {/* EVENT NAME */}
+                        <div className="flex flex-wrap -mx-3 mb-5">
+                            <div className="w-full px-3">
+                                <InputLabel
+                                    className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="eventName"
+                                >
+                                    Event Name
+                                </InputLabel>
+                                <TextField
+                                    className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="eventName"
+                                    type="text"
+                                    value={eventName}
+                                    sx={{
+                                        "& .MuiInputBase-input.Mui-disabled": {
+                                            WebkitTextFillColor: "black",
+                                        },
+                                    }}
+                                    disabled
+                                />
+                            </div>
+                        </div>
+
+                        {/* EVENT VENUE */}
+                        <div className="flex flex-wrap -mx-3 mb-5">
+                            <div className="w-full px-3">
+                                <InputLabel
+                                    className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="eventVenue"
+                                >
+                                    Venue
+                                </InputLabel>
+                                <TextField
+                                    className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="eventVenue"
+                                    type="text"
+                                    value={eventVenue}
+                                    sx={{
+                                        "& .MuiInputBase-input.Mui-disabled": {
+                                            WebkitTextFillColor: "black",
+                                        },
+                                    }}
+                                    disabled
+                                />
+                            </div>
+                        </div>
+
+                        {/* EVENT DATETIME and TICKET PRICE */}
+                        <div className="flex flex-wrap -mx-3 mb-5">
+                            {/* EVENT DATETIME */}
+                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <InputLabel
+                                    className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="eventDatetime"
+                                >
+                                    Date & time
+                                </InputLabel>
+                                <TextField
+                                    className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="eventDatetime"
+                                    type="text"
+                                    value={dayjs(event.datetime).format('YYYY-MM-DD HH:mm')}
+                                    sx={{
+                                        "& .MuiInputBase-input.Mui-disabled": {
+                                            WebkitTextFillColor: "black",
+                                        },
+                                    }}
+                                    disabled
+                                />
+                            </div>
+
+                            {/* TICKET PRICE */}
+                            <div className="w-full md:w-1/2 px-3">
+                                <InputLabel
+                                    className="block uppercase tracking-wide text-gray-700 font-bold mb-2" htmlFor="ticketPrice"
+                                >
+                                    Ticket Price
+                                </InputLabel>
+                                <div className="flex items-center">
+                                    <span className="mr-2">$</span>
+                                    <TextField
+                                        className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                        id="ticketPrice"
+                                        type="number"
+                                        value={ticketPrice}
+                                        sx={{
+                                            "& .MuiInputBase-input.Mui-disabled": {
+                                                WebkitTextFillColor: "black",
+                                            },
+                                        }}
+                                        disabled
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* SUBMIT BUTTON */}
+                        <div className="flex justify-end">
+                            <Button type="submit" variant="contained">
+                                Create Ticket
+                            </Button>
+                        </div>
+                        
+                    </Box>
+                    {/* </form> */}
                 </Container>
             </Container>
         </div>
